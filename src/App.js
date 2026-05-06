6cec0d12271b553514e0b6d97659e443f76df20c
J-esli-n <jeslinphilip001@gmail.com>
2026-05-06 21:04:44 +0530

    Remove Timeline component from App
    Removed Timeline component from App component rendering.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Publication from "./components/Publication";
 import Contact from "./components/Contact";
-import Timeline from "./components/Timeline";
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
       <Projects />
       <Publication />
       <Contact />
-      <Timeline /> 
     </>
   );
 }
